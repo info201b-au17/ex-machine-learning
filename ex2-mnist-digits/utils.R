b4b54e3a15f6969b0ed782011f2d12f1c7780f44
Ott Toomet <otoomet@uw.edu>
2017-11-22 13:29:16 -0800

    ex2-mnist-digits/utils.R: showDigit() plotting and output improved

--- ex2-mnist-digits/utils.R
+++ ex2-mnist-digits/utils.R
@@ -4,6 +4,7 @@ showDigit <- function(data, i, col=gray(12:1/12), ...) {
    ## data: a data frame, holding the mnist digits data.
    ## i    which digit: row in the data frame
    ## col  colors (need 12 colors)
+   cat("label is", data$label[i], "\n")
    data <- data %>%
       select(-label)
                            # remove the label
@@ -12,6 +13,6 @@ showDigit <- function(data, i, col=gray(12:1/12), ...) {
                            # transform data to 28x28 matrix (upside down)
    pixels <- pixels[,28:1]
                            # flip the digit upward
-   image(z=pixels, col=col, ...)
+   image(z=pixels, col=col, asp=1, ...)
 }
 
